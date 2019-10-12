a78323378c6ecca59a37226106572a09f6e3eb05
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2019-10-12 02:30:25 +0200

    Finished and Tested all problems of PS3 Random Wals and RobotSimulation

--- PS3/ps3.py
+++ PS3/ps3.py
@@ -581,5 +581,5 @@ def show_plot_room_shape(title, x_label, y_label):
     pylab.show()
 
 
-#show_plot_compare_strategies('Time to clean 80% of a 20x20 room, for various numbers of robots','Number of robots','Time / steps')
+show_plot_compare_strategies('Time to clean 80% of a 20x20 room, for various numbers of robots','Number of robots','Time / steps')
 show_plot_room_shape('Time to clean 80% of a 300-tile room for various room shapes','Aspect Ratio', 'Time / steps')
